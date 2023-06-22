ed393d6163625e4cd4d8cf8b240097e36990342c
Yeray <yeraytm01@gmail.com>
2023-06-23 01:04:06 +0200

    Window set to not resizable

--- GraphicsEngine/src/platform.cpp
+++ GraphicsEngine/src/platform.cpp
@@ -163,8 +163,9 @@ int main()
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
     glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);
 
